1edee5a9f31b95e54b8ed364e1f4832f30c9536b
mikołaj <mikolaj.czechowski2242@gmail.com>
2022-01-23 22:44:06 +0100

    nowa metoda policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+int policz(int a,int b)
+{
+    return a+b;
+}
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "w pierwszym branchu 2+3 = " <<policz(2,3)<< std::endl;
     return 0;
 }
